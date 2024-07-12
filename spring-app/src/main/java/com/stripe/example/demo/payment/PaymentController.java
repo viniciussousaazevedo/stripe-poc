a1a43c0a9f78d6288ba23c9eba7282939698a65f
Vinícius Azevedo <sousa0240@gmail.com>
2024-07-12 19:19:41 -0300

    finaliza primeira pagina do back

---
+++ spring-app/src/main/java/com/stripe/example/demo/payment/PaymentController.java
@@ -0,0 +1,63 @@
+package com.kinsta.stripejava.backend;
+
+import com.stripe.Stripe;
+import com.stripe.example.demo.payment.CustomerUtil;
+import com.stripe.example.demo.payment.ProductDAO;
+import com.stripe.example.demo.payment.RequestDTO;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.Product;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import com.stripe.param.checkout.SessionCreateParams.LineItem.PriceData;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class PaymentController {
+
+    String STRIPE_API_KEY = System.getenv().get("STRIPE_API_KEY");
+
+    @PostMapping("/checkout/hosted")
+    String hostedCheckout(@RequestBody RequestDTO requestDTO) throws StripeException {
+
+        Stripe.apiKey = STRIPE_API_KEY;
+        String clientBaseURL = System.getenv().get("CLIENT_BASE_URL");
+
+        // Start by finding an existing customer record from Stripe or creating a new one if needed
+        Customer customer = CustomerUtil.findOrCreateCustomer(requestDTO.getCustomerEmail(), requestDTO.getCustomerName());
+
+        // Next, create a checkout session by adding the details of the checkout
+        SessionCreateParams.Builder paramsBuilder =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setCustomer(customer.getId())
+                        .setSuccessUrl(clientBaseURL + "/success?session_id={CHECKOUT_SESSION_ID}")
+                        .setCancelUrl(clientBaseURL + "/failure");
+
+        for (Product product : requestDTO.getItems()) {
+            paramsBuilder.addLineItem(
+                    SessionCreateParams.LineItem.builder()
+                            .setQuantity(1L)
+                            .setPriceData(
+                                    PriceData.builder()
+                                            .setProductData(
+                                                    PriceData.ProductData.builder()
+                                                            .putMetadata("app_id", product.getId())
+                                                            .setName(product.getName())
+                                                            .build()
+                                            )
+                                            .setCurrency(ProductDAO.getProduct(product.getId()).getDefaultPriceObject().getCurrency())
+                                            .setUnitAmountDecimal(ProductDAO.getProduct(product.getId()).getDefaultPriceObject().getUnitAmountDecimal())
+                                            .build())
+                            .build());
+        }
+
+    Session session = Session.create(paramsBuilder.build());
+
+        return session.getUrl();
+    }
+}
